7c242baa174c3a11be97205cfad2ce3c978c731c
madanyang <toganm@opensuse.org>
2013-05-04 18:07:21 +0200

    license address fix
    Fix the address of FSF
    
    Signed-off-by: madanyang <toganm@opensuse.org>

--- sawfishpager.c
+++ sawfishpager.c
@@ -19,8 +19,11 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with sawfish; see the file COPYING.   If not, write to
-   the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. */
+   along with sawfish; see the file COPYING.  If not, write to
+   the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301 USA. */
+
+
 
 #include <stdlib.h>
 #include <stdio.h>
